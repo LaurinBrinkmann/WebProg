33a60168c14a85f28ab3898f8753a3ac6d227fa0
Laurin Brinkmann <l.brinkmann123@gmail.com>
2023-10-13 16:02:53 +0200

    Zebacode added Barcode scanner component

---
+++ Zebacode/src/components/CameraScanner.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+import { StreamBarcodeReader } from "vue-barcode-reader";
+
+const decodedText = ref("");
+
+const onLoaded = () => {
+  console.log("loaded");
+};
+
+const onDecode = (text) => {
+  decodedText.value = text;
+};
+</script>
+
+<template>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+  <h2>The decoded value in QR/barcode is</h2>
+  <h2>{{ decodedText }}</h2>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+.information {
+  margin-top: 100px;
+}
+</style>
--- Zebacode/src/views/Home.vue
+++ Zebacode/src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
+  <h1>Zebacode</h1>
+  <CameraScanner />
 </template>
 
 <script setup>
+  import CameraScanner from '../components/CameraScanner.vue'
 </script>
